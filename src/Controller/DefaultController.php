50ac8cf584c9efe2caf9c563003be4ecaaf4b62a
Nicolas Trugeon <ntrugeon@univ-lr.fr>
2023-12-13 11:45:53 +0100

    fichiers de départ pour l'éval

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route("/routeAModifier", name:"homepage")]
+    #[Route("/routeAModifier", name:"app_homepage")]
     public function index(): Response
     {
         return $this->render('default/index.html.twig', []);
